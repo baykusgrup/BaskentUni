e9e480b7e3626d7e73c491f846e293ad2ae4a7e5
Akın Ayturan <akinayturan@gmail.com>
2015-03-02 19:50:53 +0200

    plus

--- app/src/main/java/app/z0nen/slidemenu/menu2_Fragment.java
+++ app/src/main/java/app/z0nen/slidemenu/menu2_Fragment.java
@@ -1,21 +1,111 @@
 package app.z0nen.slidemenu;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-/**
- * Created by Z0NEN on 10/22/2014.
- */
 public class menu2_Fragment extends Fragment {
-    View rootview;
+    View rootView;
+    WebView web;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.menu2_layout, container, false);
-        return rootview;
+        rootView = inflater.inflate(R.layout.menu2_layout, container, false);
+
+        web = (WebView) rootView.findViewById(R.id.webViewBuobs);
+        web.setWebViewClient(new myWebClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("https://angora.baskent.edu.tr/buobs");
+
+        web.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        web.getSettings().setUseWideViewPort(true);
+        web.getSettings().setLoadWithOverviewMode(true);
+        web.getSettings().setSupportZoom(true);
+        web.getSettings().setBuiltInZoomControls(true);
+
+
+/*
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                super.shouldOverrideUrlLoading(view, url);
+
+                if(url!=null&&(URLUtil.isHttpsUrl(url)|| URLUtil.isHttpUrl(url)))
+                {
+                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    return true;
+                }
+                return false;
+            }});
+*/
+
+/*
+        WebView webview = (WebView)  rootView.findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://angora.baskent.edu.tr/buobs/");
+        webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setLoadWithOverviewMode(true);
+*/
+
+/*
+        webview.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl("https://angora.baskent.edu.tr/buobs/");
+                return true;
+            }});
+
+*/
+
+/*
+        String url ="https://angora.baskent.edu.tr/buobs/";
+        WebView wv=(WebView) rootView.findViewById(R.id.webView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setPluginState(WebSettings.PluginState.ON);
+        wv.getSettings().setAllowFileAccess(true);
+        wv.loadUrl(url);
+*/
+
+
+
+        return rootView;
     }
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
+    }
+/*
+    // To handle "Back" key press event for WebView to go back to previous screen.
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack()) {
+            web.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+*/
 }
--- app/src/main/java/app/z0nen/slidemenu/menu3_Fragment.java
+++ app/src/main/java/app/z0nen/slidemenu/menu3_Fragment.java
@@ -1,21 +1,54 @@
 package app.z0nen.slidemenu;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by Z0NEN on 10/22/2014.
  */
 public class menu3_Fragment extends Fragment {
-    View rootview;
+    View rootView;
+    WebView web2;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.menu3_layout, container, false);
-        return rootview;
+        rootView = inflater.inflate(R.layout.menu3_layout, container, false);
+        web2 = (WebView) rootView.findViewById(R.id.webViewMoodle);
+        web2.setWebViewClient(new myWebClient());
+        web2.getSettings().setJavaScriptEnabled(true);
+        web2.loadUrl("http://moodle.baskent.edu.tr/");
+
+        web2.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        web2.getSettings().setUseWideViewPort(true);
+        web2.getSettings().setLoadWithOverviewMode(true);
+        web2.getSettings().setSupportZoom(true);
+        web2.getSettings().setBuiltInZoomControls(true);
+
+        return rootView;
+    }
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
     }
 }
