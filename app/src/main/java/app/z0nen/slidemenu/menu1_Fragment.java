83b15cfdaf4817ef7f15dc3c0f9eafa09aedd9c4
Akın Ayturan <akinayturan@gmail.com>
2015-03-02 20:43:48 +0200

    edit

--- app/src/main/java/app/z0nen/slidemenu/menu1_Fragment.java
+++ app/src/main/java/app/z0nen/slidemenu/menu1_Fragment.java
@@ -1,21 +1,95 @@
 package app.z0nen.slidemenu;
 
 import android.app.Fragment;
+import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-/**
- * Created by Z0NEN on 10/22/2014.
- */
-public class menu1_Fragment extends Fragment {
-    View rootview;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+public class menu1_Fragment extends ListFragment {
+    // All static variables
+    static final String URL = "http://api.androidhive.info/pizza/?format=xml";
+    // XML node keys
+    static final String KEY_ITEM = "item"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_NAME = "name";
+    static final String KEY_COST = "cost";
+    static final String KEY_DESC = "description";
+
+
+    View rootView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootview = inflater.inflate(R.layout.menu1_layout, container, false);
-        return rootview;
+        rootView = inflater.inflate(R.layout.menu1_layout, container, false);
+
+
+        ArrayList<HashMap<String, String>> menuItems = new ArrayList<HashMap<String, String>>();
+
+        XMLParser parser = new XMLParser();
+        String xml = parser.getXmlFromUrl(URL); // getting XML
+        Document doc = parser.getDomElement(xml); // getting DOM element
+
+        NodeList nl = doc.getElementsByTagName(KEY_ITEM);
+        // looping through all item nodes <item>
+        for (int i = 0; i < nl.getLength(); i++) {
+            // creating new HashMap
+            HashMap<String, String> map = new HashMap<String, String>();
+            Element e = (Element) nl.item(i);
+            // adding each child node to HashMap key => value
+            map.put(KEY_ID, parser.getValue(e, KEY_ID));
+            map.put(KEY_NAME, parser.getValue(e, KEY_NAME));
+            map.put(KEY_COST, "Rs." + parser.getValue(e, KEY_COST));
+            map.put(KEY_DESC, parser.getValue(e, KEY_DESC));
+
+            // adding HashList to ArrayList
+            menuItems.add(map);
+        }
+
+
+        // Adding menuItems to ListView
+        ListAdapter adapter = new SimpleAdapter(this, menuItems, R.id.list_item,
+                new String[] { KEY_NAME, KEY_DESC, KEY_COST }, new int[] {
+                R.id.name, R.id.desciption, R.id.cost });
+
+        setListAdapter(adapter);
+
+        // selecting single ListView item
+        ListView lv = getListView();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String name = ((TextView) view.findViewById(R.id.name)).getText().toString();
+                String cost = ((TextView) view.findViewById(R.id.cost)).getText().toString();
+                String description = ((TextView) view.findViewById(R.id.desciption)).getText().toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(), SingleMenuItemActivity.class);
+                in.putExtra(KEY_NAME, name);
+                in.putExtra(KEY_COST, cost);
+                in.putExtra(KEY_DESC, description);
+                startActivity(in);
+
+            }
+        });
+        return rootView;
     }
 }
